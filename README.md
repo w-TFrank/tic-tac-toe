7e16ee80ae6923c83f6d536864016ccbdede8250
TFrank <madmonsterHI@gmail.com>
2023-08-08 16:17:49 -0400

    added comments, readme, cleaned code, fixed bug w/ win cons

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tic-tac-toe
+
+Lots of practice in this one with manipulating the DOM and event listeners. Also working with arrays. Had to think a lot in this project about what order the different functions need to be executed in, and how they all need to be organized within and around each other. 
+
+This project also showed me that I need to pre plan these things out more. It would've gone a lot smoother.
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,30 +1,52 @@
+function getPlayers() {
+    //adds event listeners to the x & o button so p1 can choose what they want
+    //calls game controller with whichever symbol they choose
+    const choose = document.getElementById("choose-symbols");
+    const xButton = document.getElementById("x-button");
+    const oButton = document.getElementById("o-button");
+    //so the choice reappears after play again is hit
+    choose.style.display = "block";
+    xButton.addEventListener("click", handler);
+    oButton.addEventListener("click", handler);
+    function handler() {
+        xButton.removeEventListener("click", handler);
+        oButton.removeEventListener("click", handler);
+        choose.style.display = "none";
+        if (this.id === "x-button") {
+            GameController("X");
+        } else {
+            GameController("O");
+        }
+    }
+}
 function GameController(symbolChoice) {
     //gets an array of squares for the game board
     const square = document.getElementsByClassName("square");
 
-    //sets the starting player as whatever the player chose
-    let currentPlayer = (symbolChoice === "O") ? "p2" : "p1";
+    //player 1 goes first
+    let currentPlayer = "p1";
 
     function play(e) {
         //checks to see if square already has value,
         //then checks for win,
         //then checks for tie,
         //if either are true then event listeners are turned off
-        //if not then current player is switched and play continues
+        //if not then current player  & symbol is switched and play continues
         if (e.target.value === null) {
-            playRound(e, currentPlayer);
+            playRound(e, currentPlayer, symbolChoice);
             if (checkForWin(square)) {
                 stopPlay(currentPlayer, "win");
             } else if (checkForTie(square)) {
                 stopPlay(currentPlayer, "tie");
             }
             currentPlayer = (currentPlayer === "p1") ? "p2" : "p1";
+            symbolChoice = (symbolChoice === "X") ? "O" : "X";
         }
     }
-
     function stopPlay(currentPlayer, winCondition) {
         //removes all the event listeners so squares can't continue to be clicked
-        //after a win or tie has been reached
+        //after a win or tie has been reached.
+        //also prints a win message for either player, or a tie message
         currentPlayer = (currentPlayer === "p1") ? "Player 1" : "Player 2";
         if (winCondition === "win") {
             const chooseSymbols = document.getElementById("win-con");
@@ -47,9 +69,9 @@ function GameController(symbolChoice) {
         }
         playAgain();
     }
-
     function playAgain() {
         //makes the play again button become visible and adds a listener to the button
+        //get the win element so it can be removed later
         const playAgain = document.getElementById("play-again");
         const win = document.getElementById("win");
         playAgain.style.display = "flex";
@@ -59,6 +81,7 @@ function GameController(symbolChoice) {
             //if the button is clicked, the button is hidden again, the listener
             //is turned off, and the values of all the squares are reset so the game
             //can be played again. play restarts
+            //removes the win message
             playAgain.style.display = "none";
             win.remove();
             for (let i = 0; i < square.length; i++) {
@@ -70,43 +93,15 @@ function GameController(symbolChoice) {
             getPlayers();
         }
     }
-
     //adds event listeners to each of the squares
     for (let i = 0; i < square.length; i++) {
         square[i].value = null;
         square[i].addEventListener("click", play);
     }
 }
-
-function getPlayers() {
-    const choose = document.getElementById("choose-symbols");
-    const xButton = document.getElementById("x-button");
-    const oButton = document.getElementById("o-button");
-
-    choose.style.display = "block";
-
-    xButton.addEventListener("click", xHandler);
-    oButton.addEventListener("click", oHandler);
-
-    function xHandler() {
-        GameController("X");
-        xButton.removeEventListener("click", xHandler);
-        oButton.removeEventListener("click", oHandler);
-        choose.style.display = "none";
-    }
-
-    function oHandler() {
-        GameController("O");
-        xButton.removeEventListener("click", xHandler);
-        oButton.removeEventListener("click", oHandler);
-        choose.style.display = "none";
-    }
-}
-
-function playRound(e, currentPlayer) {
+function playRound(e, currentPlayer, symbolChoice) {
     //sets the value of the selected square to whoever the current player is
     e.target.value = currentPlayer;
-
     //creates the symbols that'll be in the square
     const xSymbol = document.createElement("div");
     const oSymbol = document.createElement("div");
@@ -114,16 +109,13 @@ function playRound(e, currentPlayer) {
     oSymbol.className = "o-square";
     xSymbol.textContent = "X";
     oSymbol.textContent = "O";
-
     //sets the correct symbol into the square
-    let symbol = (currentPlayer === "p1") ? "X" : "O";
-    if (symbol === "X") {
+    if (symbolChoice === "X") {
         e.target.appendChild(xSymbol);
     } else {
         e.target.appendChild(oSymbol);
     }
 }
-
 function checkForWin(square) {
     //all the win conditions
     if (areEqual(square[0].value, square[1].value, square[2].value) ||
@@ -138,7 +130,6 @@ function checkForWin(square) {
     }
     return false;
 }
-
 function checkForTie(square) {
     //if all 9 squares !== null, and no win has been detected already, it must be a tie
     if (areFull(square[0].value, square[1].value, square[2].value,
@@ -148,7 +139,6 @@ function checkForTie(square) {
     }
     return false;
 }
-
 function areEqual() {
     //makes sure that the tile values aren't null (blank) and that they're equal (3 X's or O's)
     for (let i = 1; i < arguments.length; i++) {
@@ -158,13 +148,8 @@ function areEqual() {
     }
     return true;
 }
-
 function areFull() {
     //checks if values are null
-
-    //i think this could be gotten rid of and it could just be done
-    //in checkForTie if statement pretty easily
-    //but this works
     for (let i = 0; i < arguments.length; i++) {
         if (arguments[i] === null) {
             return false
@@ -172,5 +157,4 @@ function areFull() {
     }
     return true;
 }
-
 getPlayers();
